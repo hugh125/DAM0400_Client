b995a4ad14af8b65c9adca9f02460190295c92b3
zhoukui <zhoukui655@163.com>
2018-08-28 15:26:15 +0800

    调试控制器，修改接口方法，传入IP地址、端口号、开关变量

--- src/Client.java
+++ src/Client.java
@@ -3,8 +3,16 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
+    boolean isOK=false;
 
-    public void OpenClient(String strIP, int iPort) throws Exception {
+    /**
+     * 输入IP地址、端口号、开关命令，实现对控制器的开关控制
+     * @param strIP 控制器IP地址 具体信息找控制器设置人员，询问IP地址
+     * @param iPort 控制器端口号 默认60001 具体信息找控制器设置人员，询问端口号
+     * @param isOpen 控制器开关
+     * @throws Exception
+     */
+    public void OpenClient(String strIP, int iPort, boolean isOpen) throws Exception {
         //客户端请求与本机在 iPort 端口建立TCP连接
         Socket client = new Socket(strIP, iPort);
         client.setSoTimeout(10 * 1000);
@@ -14,13 +22,13 @@ public class Client {
         OutputStream out = client.getOutputStream();
         try {
             //发送字节流
-            byte[] bytesSend = WriteAllDO(0xfe, true);
+            byte[] bytesSend = WriteAllDO(0xfe, isOpen);
             out.write(bytesSend);
 
             //接收字节流
             byte[] bytesRecv = new byte[10];
             in.read(bytesRecv);
-            boolean isOK = ReadAllDO(bytesRecv);
+            isOK = ReadAllDO(bytesRecv);
 
         } finally {
             //3.使用Socet类的close（）方法关闭连接
@@ -32,7 +40,28 @@ public class Client {
 
     /**
      * 发送开关指令
-     *
+     * @param addr      IP地址最后一位
+     * @param io        要控制继电器寄存器地址
+     * @param openclose 开关状态
+     * @return
+     */
+    public byte[] WriteDO(int addr,int io,boolean openclose)
+    {
+        byte[] src = new byte[8];
+        src[0] = (byte)addr;
+        src[1] = 0x05;
+        src[2] = 0x00;
+        src[3] = (byte)io;
+        src[4] = (byte)((openclose) ? 0xff : 0x00);
+        src[5] = 0x00;
+        char crc = CMBRTU.CalculateCrc(src,6);
+        src[6] = (byte)(crc & 0xff);
+        src[7] = (byte)(crc>>8);
+        return src;
+    }
+
+    /**
+     * 发送开关指令
      * @param addr      IP地址最后一位
      * @param openclose 开关状态
      * @return 返回要发送的字节流
@@ -60,6 +89,10 @@ public class Client {
      * @return 返回处理后的结果
      */
     public boolean ReadAllDO(byte[] bytesRecv) {
+        for (int i = 0; i < bytesRecv.length; i++){
+            System.out.printf(Integer.toHexString(bytesRecv[i]&0xff));
+        }
+        System.out.println();
         char crcRecv, crcCurrent;
         crcRecv = CMBRTU.CalculateCrc(bytesRecv, 8);
         crcCurrent = (char) (bytesRecv[9] << 8 | bytesRecv[8]);
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
 
-        String strIP = "127.0.0.1";
-        int iPort = 6001;
+        String strIP = "192.168.1.170";
+        int iPort = 60001;
         Client client =new Client();
-        client.OpenClient(strIP, iPort);
+        client.OpenClient(strIP, iPort, true);  //模拟4端口开
+        client.OpenClient(strIP, iPort, false);  //模拟4端口关
     }
 }
 
